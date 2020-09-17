863e55c6ee8912aaa1a9d93fb5ad5215c0ea5e26
QazyBi <k.askarbek@innopolis.university>
2020-09-17 16:11:49 +0300

    threading added

--- server_threads.py
+++ server_threads.py
@@ -1,6 +1,7 @@
 import socket
 import tqdm
 import os
+from threading import Thread
 
 
 HOST = '127.0.0.1'
@@ -8,32 +9,41 @@ PORT = 8800
 BUFFER_SIZE = 4096
 SEPARATOR = "<SEPARATOR>"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print(f"[*] Listening as {HOST}:{PORT}")
-    conn, addr = s.accept()
-    with conn:
-        print(f"[+] {addr} is connected.")
-        received = conn.recv(BUFFER_SIZE).decode()
+clients = []
+
+
+def get_available_filename(filename):
+    new_filename = filename
+    copy_num = 0
+    basename, *extension = filename.split('.')
+    extension = '.'.join(extension)
+    while os.path.isfile(new_filename):
+        copy_num += 1
+        new_filename = f"{basename}_copy{copy_num}.{extension}"
+
+    return new_filename
+
+
+class ClientListener(Thread):
+    def __init__(self, sock: socket.socket):
+        super().__init__(daemon=True)
+        self.sock = sock
+
+    def run(self):
+        # print(f"[+] {addr} is connected.")
+        received = self.sock.recv(BUFFER_SIZE).decode()
         filename, filesize = received.split(SEPARATOR)
         filename = os.path.basename(filename)
         filesize = int(filesize)
 
         progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024, leave=False)
 
-        new_filename = filename
-        copy_num = 0
-        basename, *extension = new_filename.split('.')
-        extension = '.'.join(extension)
-        while os.path.isfile(new_filename):
-            copy_num += 1
-            new_filename = f"{basename}_copy{copy_num}.{extension}"
+        new_filename = get_available_filename(filename)
 
         with open(new_filename, "wb") as f:
             for _ in progress:
                 # read 1024 bytes from the socket (receive)
-                bytes_read = conn.recv(BUFFER_SIZE)
+                bytes_read = self.sock.recv(BUFFER_SIZE)
                 if not bytes_read:
                     # nothing is received
                     # file transmitting is done
@@ -43,7 +53,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 # update the progress bar
                 progress.update(len(bytes_read))
 
+        print(f"\n[*] File:{filename} Successfully Received\n")
         # close the client socket
-        conn.close()
-        # close the server socket
-        s.close()
+        self.sock.close()
+
+
+if __name__ == "__main__":
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print(f"[*] Listening as {HOST}:{PORT}")
+
+        while True:
+            conn, addr = s.accept()
+            print(f"[*] Connected to {addr}")
+            clients.append(conn)
+            ClientListener(conn).start()
