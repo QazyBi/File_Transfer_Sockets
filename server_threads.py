e4ec45a06e7285c85573713a6ec7ff53dfd6d601
QazyBi <k.askarbek@innopolis.university>
2020-09-17 15:06:36 +0300

    host and port passed by cli argument

--- server_threads.py
+++ server_threads.py
@@ -9,33 +9,41 @@ BUFFER_SIZE = 4096
 SEPARATOR = "<SEPARATOR>"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	s.bind((HOST, PORT))
-	s.listen()
-	print(f"[*] Listening as {HOST}:{PORT}")
-	conn, addr = s.accept()
-	with conn:
-		print(f"[+] {addr} is connected.")
-		received = conn.recv(BUFFER_SIZE).decode()
-		filename, filesize = received.split(SEPARATOR)
-		filename = os.path.basename(filename)
-		filesize = int(filesize)
-		
-		progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024, leave=False)
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"[*] Listening as {HOST}:{PORT}")
+    conn, addr = s.accept()
+    with conn:
+        print(f"[+] {addr} is connected.")
+        received = conn.recv(BUFFER_SIZE).decode()
+        filename, filesize = received.split(SEPARATOR)
+        filename = os.path.basename(filename)
+        filesize = int(filesize)
 
-		with open(filename, "wb") as f:
-		    for _ in progress:
-		        # read 1024 bytes from the socket (receive)
-		        bytes_read = conn.recv(BUFFER_SIZE)
-		        if not bytes_read:    
-		            # nothing is received
-		            # file transmitting is done
-		            break
-		        # write to the file the bytes we just received
-		        f.write(bytes_read)
-		        # update the progress bar
-		        progress.update(len(bytes_read))
+        progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024, leave=False)
 
-		# close the client socket
-		conn.close()
-		# close the server socket
-		s.close()						
+        new_filename = filename
+        copy_num = 0
+        basename, *extension = new_filename.split('.')
+        extension = '.'.join(extension)
+        while os.path.isfile(new_filename):
+            copy_num += 1
+            new_filename = f"{basename}_copy{copy_num}.{extension}"
+
+        with open(new_filename, "wb") as f:
+            for _ in progress:
+                # read 1024 bytes from the socket (receive)
+                bytes_read = conn.recv(BUFFER_SIZE)
+                if not bytes_read:
+                    # nothing is received
+                    # file transmitting is done
+                    break
+                # write to the file the bytes we just received
+                f.write(bytes_read)
+                # update the progress bar
+                progress.update(len(bytes_read))
+
+        # close the client socket
+        conn.close()
+        # close the server socket
+        s.close()
