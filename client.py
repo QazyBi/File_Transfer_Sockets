f93e9e5c4e912bdeb7d009ee249631fafd8bc366
QazyBi <k.askarbek@innopolis.university>
2020-09-17 16:53:24 +0300

    comments added, fixed issue with remaining progress bars

--- client.py
+++ client.py
@@ -1,40 +1,53 @@
+'''
+Author: Kazybek Askarbek
+Date: 17.09.20
+Description: File uploading client
+'''
+
 import socket
 import tqdm
 import os
 import sys
 
 
-HOST = sys.argv[2]
-PORT = int(sys.argv[3])
+# Global variables
 BUFFER_SIZE = 4096
 SEPARATOR = "<SEPARATOR>"
 
+
+# handle exceptions
 try:
     filename = sys.argv[1]
+    HOST = sys.argv[2]
+    PORT = int(sys.argv[3])
+
     filesize = os.path.getsize(filename)
 except OSError:
     print("File not found")
     sys.exit()
 except IndexError:
-    print("No filename provided")
+    print("Not enough arguments passed")
+    print("Please provide: filename hostname port")
     sys.exit()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # connect to the server
     s.connect((HOST, PORT))
+    # send filename and filesize to the server
     s.send(f'{filename}{SEPARATOR}{filesize}'.encode())
 
+    # initialize progress bar
     progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024, leave=False)
     with open(filename, "rb") as f:
         for _ in progress:
             # read the bytes from the file
             bytes_read = f.read(BUFFER_SIZE)
             if not bytes_read:
-                # file transmitting is done
+                # if no bytes read then stop
                 break
-            # we use sendall to assure transimission in
-            # busy networks
+            # send file to the server
             s.sendall(bytes_read)
-            # update the progress bar
+            # update progress bar
             progress.update(len(bytes_read))
     # close the socket
     s.close()
--- server_threads.py
+++ server_threads.py
@@ -1,56 +1,68 @@
+'''
+Author: Kazybek Askarbek
+Date: 17.09.20
+Description: File receiving server
+'''
+
 import socket
 import tqdm
 import os
 from threading import Thread
 
 
+# Global Variables
 HOST = '127.0.0.1'
 PORT = 8800
 BUFFER_SIZE = 4096
 SEPARATOR = "<SEPARATOR>"
 
-clients = []
 
-
+# functions to find available filenames
 def get_available_filename(filename):
     new_filename = filename
+    # variable to store number of copies
     copy_num = 0
+    # get basename and extension of the file
     basename, *extension = filename.split('.')
+    # in case file format of compound type join them into one string
     extension = '.'.join(extension)
     while os.path.isfile(new_filename):
         copy_num += 1
+        # update filename
         new_filename = f"{basename}_copy{copy_num}.{extension}"
 
     return new_filename
 
 
+# Class for Individual Thread Connections with Clients
 class ClientListener(Thread):
     def __init__(self, sock: socket.socket):
         super().__init__(daemon=True)
+        # assign received socket
         self.sock = sock
 
+    # functions that will run the thread
     def run(self):
-        # print(f"[+] {addr} is connected.")
+        # receive filename and filesize
         received = self.sock.recv(BUFFER_SIZE).decode()
         filename, filesize = received.split(SEPARATOR)
+        # strip path from filename
         filename = os.path.basename(filename)
         filesize = int(filesize)
 
+        # initialize progress bar
         progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024, leave=False)
 
+        # get available filename
         new_filename = get_available_filename(filename)
 
+        # receive and write data to a new file
         with open(new_filename, "wb") as f:
             for _ in progress:
-                # read 1024 bytes from the socket (receive)
                 bytes_read = self.sock.recv(BUFFER_SIZE)
                 if not bytes_read:
-                    # nothing is received
-                    # file transmitting is done
                     break
-                # write to the file the bytes we just received
                 f.write(bytes_read)
-                # update the progress bar
                 progress.update(len(bytes_read))
 
         print(f"\n[*] File:{filename} Successfully Received\n")
@@ -60,12 +72,15 @@ class ClientListener(Thread):
 
 if __name__ == "__main__":
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # bind socket with HOST ip number and PORT number
         s.bind((HOST, PORT))
+        # listen for connections
         s.listen()
         print(f"[*] Listening as {HOST}:{PORT}")
 
         while True:
+            # if connection established
             conn, addr = s.accept()
             print(f"[*] Connected to {addr}")
-            clients.append(conn)
+            # start thread execution
             ClientListener(conn).start()
